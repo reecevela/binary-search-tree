a36a4ce3a147743ac1c3361ec1f9312d84d75b00
reecevela <reecevela@outlook.com>
2023-03-17 14:37:01 -0500

    FINALLY FINISH!

--- src/Tree.js
+++ src/Tree.js
@@ -71,15 +71,192 @@ export default class Tree {
         }
     }
     delete(data) {
-        const root = this.root;
-        if (root === null) {
+        if (this.root === null) {
             return;
         }
-        let deletedNode = null;
-        let nodesToCheck = [root];
-        if (this.root.data == data) {
-            deletedNode = this.root;
+    
+        let current = this.root;
+        let parent = null;
+        let isLeftChild = false;
+    
+        // Find the node to delete and its parent
+        while (current && current.data !== data) {
+            parent = current;
+            if (data < current.data) {
+                current = current.left;
+                isLeftChild = true;
+            } else {
+                current = current.right;
+                isLeftChild = false;
+            }
         }
-        //where I left off today
+    
+        // If the node is not found
+        if (current === null) {
+            return;
+        }
+    
+        // Case 1: Node with no children
+        if (!current.left && !current.right) {
+            if (current === this.root) {
+                this.root = null;
+            } else if (isLeftChild) {
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        }
+        // Case 2: Node with one child (left child)
+        else if (!current.right) {
+            if (current === this.root) {
+                this.root = current.left;
+            } else if (isLeftChild) {
+                parent.left = current.left;
+            } else {
+                parent.right = current.left;
+            }
+        }
+        // Case 3: Node with one child (right child)
+        else if (!current.left) {
+            if (current === this.root) {
+                this.root = current.right;
+            } else if (isLeftChild) {
+                parent.left = current.right;
+            } else {
+                parent.right = current.right;
+            }
+        }
+        // Case 4: Node with two children
+        else {
+            // Find the minimum value node in the right subtree
+            let minRight = current.right;
+            let minRightParent = current;
+    
+            while (minRight.left) {
+                minRightParent = minRight;
+                minRight = minRight.left;
+            }
+    
+            // Replace the current node's data with the minimum value node's data
+            current.data = minRight.data;
+    
+            // Remove the minimum value node from the right subtree
+            if (minRightParent === current) {
+                minRightParent.right = minRight.right;
+            } else {
+                minRightParent.left = minRight.right;
+            }
+        }
     }
+    
+    find(value) {
+        let current = this.root;
+        while (current) {
+            if (value === current.data) {
+                return current;
+            } else if (value < current.data) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return null;
+    }
+    levelOrder(fn) {
+        let result = [];
+        let queue = [this.root];
+    
+        while (queue.length) {
+            let node = queue.shift();
+            if (fn) {
+                fn(node);
+            } else {
+                result.push(node.data);
+            }
+    
+            if (node.left) {
+                queue.push(node.left);
+            }
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+        return result;
+    }
+    inorder(fn, node = this.root) {
+        let result = [];
+    
+        if (node) {
+            result = result.concat(this.inorder(fn, node.left));
+            if (fn) {
+                fn(node);
+            } else {
+                result.push(node.data);
+            }
+            result = result.concat(this.inorder(fn, node.right));
+        }
+        return result;
+    }
+    preorder(fn, node = this.root) {
+        let result = [];
+    
+        if (node) {
+            if (fn) {
+                fn(node);
+            } else {
+                result.push(node.data);
+            }
+            result = result.concat(this.preorder(fn, node.left));
+            result = result.concat(this.preorder(fn, node.right));
+        }
+        return result;
+    }
+    postorder(fn, node = this.root) {
+        let result = [];
+    
+        if (node) {
+            result = result.concat(this.postorder(fn, node.left));
+            result = result.concat(this.postorder(fn, node.right));
+            if (fn) {
+                fn(node);
+            } else {
+                result.push(node.data);
+            }
+        }
+        return result;
+    }
+    height(node) {
+        if (!node) {
+            return -1;
+        }
+        return 1 + Math.max(this.height(node.left), this.height(node.right));
+    }
+    depth(node, current = this.root) {
+        if (!node || !current) {
+            return -1;
+        }
+        if (node.data < current.data) {
+            return 1 + this.depth(node, current.left);
+        } else if (node.data > current.data) {
+            return 1 + this.depth(node, current.right);
+        } else {
+            return 0;
+        }
+    }
+    isBalanced(node = this.root) {
+        if (!node) {
+            return true;
+        }
+        const leftHeight = this.height(node.left);
+        const rightHeight = this.height(node.right);
+        return (
+            Math.abs(leftHeight - rightHeight) <= 1 &&
+            this.isBalanced(node.left) &&
+            this.isBalanced(node.right)
+        );
+    }
+    rebalance() {
+        const nodes = this.inorder();
+        this.root = this.buildTree(nodes);
+    }    
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,39 @@ import Tree from './Tree';
 import prettyPrint from './PrettyPrint';
 const {log} = console;
 
-const inputArray = [95, 52, 98, 90, 61, 94, 6, 64, 44, 26, 88, 82];
+function generateRandomNumbers(size) {
+    const arr = [];
+    for (let i = 0; i < size; i++) {
+    arr.push(Math.floor(Math.random() * 100));
+    }
+    return arr;
+}
 
-const testTree = new Tree(inputArray);
+const randomNumbers = generateRandomNumbers(25);
+const testTree = new Tree(randomNumbers);
 
 log(testTree.root);
 
 prettyPrint(testTree.root);
+
+log("Is the tree balanced?", testTree.isBalanced());
+log("Level order:", testTree.levelOrder());
+log("Preorder:", testTree.preorder());
+log("Post order:", testTree.postorder());
+log("In order:", testTree.inorder());
+
+// Unbalance the tree
+testTree.insert(101);
+testTree.insert(102);
+testTree.insert(103);
+testTree.insert(104);
+testTree.insert(105);
+
+// Rebalance the tree
+testTree.rebalance();
+
+log("Is the tree balanced after rebalancing?", testTree.isBalanced());
+log("Level order after rebalancing:", testTree.levelOrder());
+log("Pre order after rebalancing:", testTree.preorder());
+log("Post order after rebalancing:", testTree.postorder());
+log("In order after rebalancing:", testTree.inorder());
